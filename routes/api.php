cc0f0d43fca03d6d8d07b0e5dcf57c793bccbba8
jeronimoalbano <jeronimo.jalbano@opovodigital.com>
2021-05-20 10:58:40 -0300

    chore: atualização dos endpoints

--- routes/api.php
+++ routes/api.php
@@ -18,10 +18,17 @@ Route::post('auth/login', 'App\\Http\\Controllers\\Api\\AuthController@login');
 
 
 Route::group(['middleware' =>['apiUsers']],function(){
-    // Route::post('auth/registerTeste', 'App\\Http\\Controllers\\Api\\RegisterController@create');    
+    Route::post('auth/measures/insert', 'App\\Http\\Controllers\\Api\\MeasuresController@create');
+    Route::post('auth/measurer', 'App\\Http\\Controllers\\Api\\MeasurersController@index');
+    Route::post('auth/measurer/measures', 'App\\Http\\Controllers\\Api\\MeasuresController@getDay');
+    Route::post('auth/measurer/measures-month', 'App\\Http\\Controllers\\Api\\MeasuresController@getMonth');
+    Route::post('auth/measurer/measures-week', 'App\\Http\\Controllers\\Api\\MeasuresController@getWeek');
+    Route::post('auth/measurer/measures-records', 'App\\Http\\Controllers\\Api\\MeasuresController@records');
 });
 
 Route::group(['middleware' =>['apiAdms']], function(){
     Route::post('auth/register', 'App\\Http\\Controllers\\Api\\RegisterController@create');
+    Route::post('auth/users', 'App\\Http\\Controllers\\Api\\RegisterController@show');
+    Route::post('auth/measurer/insert', 'App\\Http\\Controllers\\Api\\MeasurersController@create');
 });
 
