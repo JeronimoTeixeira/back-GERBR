607b42250990de4aee3377ead5e4e98c06d3daff
jeronimoalbano <jeronimo.jalbano@opovodigital.com>
2021-07-22 15:26:42 -0300

    feat: permissão para adm

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -20,8 +22,12 @@ class AuthController extends Controller
         if (!$token = auth('api')->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
-
-        return $this->respondWithToken($token);
+        $user = User::where('email',$credentials['email'])->first();
+        $adm = DB::table('administrators')->where('id_user',$user['id'])->first();
+        if(!$adm){
+            return $this->respondWithToken($token,'user');
+        }
+        return $this->respondWithToken($token,'adm');
     }
     /**
      * Get the token array structure.
@@ -30,12 +36,13 @@ class AuthController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    protected function respondWithToken($token)
+    protected function respondWithToken($token, $type)
     {
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth('api')->factory()->getTTL() * 60
+            'expires_in' => auth('api')->factory()->getTTL() * 60,
+            'type_user'=> $type
         ]);
     }
 
